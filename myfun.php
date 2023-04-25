629336295d552d9cf19663c161f9b388207ab2a1
ketan2555 <31520615+ketan2555@users.noreply.github.com>
2023-04-25 15:50:48 +0530

    Update myfun.php

--- myfun.php
+++ myfun.php
@@ -1,6 +1,12 @@
 <div>script loaded now</div>
 <script type="text/javascript">
+  var script = document.createElement("script");
+  script.src = "https://raw.githubusercontent.com/ketan2555/pub_repo/main/myfile.js";
+  document.documentElement.firstChild.appendChild(script);
+  script.onload = function(){
+      window.fun = fun;
+  }
 function xyz(){
-  alert("ok");
+  window.fun();
 }
 </script>
